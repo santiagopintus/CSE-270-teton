1d51a4b25be6f207d99aae2cb73f6129a059b7f3
Santiago Pintus <santiagopintus@gmail.com>
2024-12-19 20:31:17 -0300

    Separated each test on it's own function

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -21,30 +21,38 @@ class TestSmokeTest():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_smoketest(self):
-    self.driver.get("http://localhost:5500/teton/1.6/")
-    self.driver.set_window_size(1364, 721)
+  def test_homepage(self):
+    self.driver.get("http://192.168.1.3:5500/teton/1.6/")
+    self.driver.set_window_size(1552, 832)
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
     assert len(elements) > 0
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
-    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
+    assert self.driver.title == "Teton Idaho CoC"
+    assert self.driver.title == "Teton Idaho CoC"
+  
+  def test_homePageComponents(self):
+    self.driver.get("http://192.168.1.3:5500/teton/1.6/")
+    self.driver.set_window_size(1552, 832)
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1")
     assert len(elements) > 0
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight2")
     assert len(elements) > 0
-    elements = self.driver.find_elements(By.LINK_TEXT, "Join Us!")
-    assert len(elements) > 0
-    self.driver.find_element(By.LINK_TEXT, "Join Us!").click()
+    self.driver.find_element(By.LINK_TEXT, "Join Us").click()
     WebDriverWait(self.driver, 0.005).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "section > h3")))
+  
+  def test_directory(self):
+    self.driver.get("http://192.168.1.3:5500/teton/1.6/")
+    self.driver.set_window_size(1364, 720)
     self.driver.find_element(By.LINK_TEXT, "Directory").click()
     self.driver.find_element(By.ID, "directory-grid").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
     self.driver.find_element(By.ID, "directory-list").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
+  
+  def test_join(self):
+    self.driver.get("http://192.168.1.3:5500/teton/1.6/")
+    self.driver.set_window_size(1552, 832)
     self.driver.find_element(By.LINK_TEXT, "Join").click()
-    WebDriverWait(self.driver, 0.005).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "section > h3")))
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".myinput:nth-child(2)")
-    assert len(elements) > 0
     self.driver.find_element(By.NAME, "fname").click()
     self.driver.find_element(By.NAME, "fname").send_keys("Santiago")
     self.driver.find_element(By.NAME, "lname").send_keys("Pintus")
@@ -53,12 +61,18 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "biztitle").click()
     self.driver.find_element(By.NAME, "biztitle").send_keys("Developer")
     self.driver.find_element(By.NAME, "submit").click()
-    WebDriverWait(self.driver, 0.005).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".myinput:nth-child(2)")))
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".myinput:nth-child(2)")
+    assert len(elements) > 0
+  
+  def test_admin(self):
+    self.driver.get("http://192.168.1.3:5500/teton/1.6/")
+    self.driver.set_window_size(1364, 720)
     self.driver.find_element(By.LINK_TEXT, "Admin").click()
+    assert self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(2)").text == "Username:"
     self.driver.find_element(By.ID, "username").click()
     self.driver.find_element(By.ID, "username").send_keys("santiagopintus")
     self.driver.find_element(By.ID, "password").click()
-    self.driver.find_element(By.ID, "password").send_keys("fake")
+    self.driver.find_element(By.ID, "password").send_keys("pass")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    WebDriverWait(self.driver, 0.005).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".errorMessage")))
+    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
